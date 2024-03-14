116cf0fa6363b59a206deb0ec3ae6adcf38bbbc5
agusev <audetv@gmail.com>
2024-03-14 15:38:30 +0300

    Added QueryString interface

--- src/lib/interfaces.ts
+++ src/lib/interfaces.ts
@@ -19,3 +19,25 @@ interface Highlight {
     content: string;
     url: string;
 }
+
+interface HighlightFields {
+    fields: string[];
+    limit: number;
+    no_match_size: number;
+    pre_tags: string;
+    post_tags: string;
+}
+
+interface QueryString {
+    index: string;
+    highlight?: HighlightFields;
+    query: {
+        query_string: string;
+        bool?: {
+            should: { equals: { language: string } }[];
+        };
+    };
+    limit: number;
+    offset: number;
+    sort?: { created: { order: string } }[];
+}
