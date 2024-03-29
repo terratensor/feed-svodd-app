d1dd1dd60df260f048ca4c5fac516aa83213e169
agusev <audetv@gmail.com>
2024-03-30 00:04:22 +0300

    Добавлен переключатель языка

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,17 @@
 import SvoddLogoIcon from "@/ui/icons/SvoddLogoIcon";
 import React from "react";
+import LocaleSwitcher from "@/components/LocaleSwitcher";
 
 export default function Footer() {
     return (
         <footer className="container mx-auto max-w-[1320px] xl:px-16 md:px-5 pt-6 pb-4">
             <div className='mx-auto px-4'>
                 <div className='flex items-center text-center justify-between '>
-                    <div className="flex items-center text-base/7 text-svoddBlack-100 dark:text-svoddWhite-200">
+                    <div className="flex items-center sm:text-base/7 text-sm text-svoddBlack-100 dark:text-svoddWhite-200">
                         <SvoddLogoIcon
                             className=" h-[16px] w-[16px] -translate-y-[6%] mr-1"/>
                         СВОДД 24.02.2022 - {new Date().getFullYear()}</div>
-                    <span>Язык</span>
+                    <LocaleSwitcher />
                 </div>
             </div>
         </footer>
--- src/components/LocaleSwitcher.tsx
+++ src/components/LocaleSwitcher.tsx
@@ -1,18 +1,64 @@
-import {useLocale, useTranslations} from 'next-intl';
+'use client'
+import {useLocale} from 'next-intl';
 import {locales} from '@/config';
-import LocaleSwitcherSelect from './LocaleSwitcherSelect';
+import {Dropdown} from '@mui/base/Dropdown';
+import {Menu} from '@mui/base/Menu';
+import {MenuButton} from '@mui/base/MenuButton';
+import React, {useTransition} from "react";
+import MenuItem from "@mui/material/MenuItem";
+import {usePathname, useRouter} from "@/navigation";
+import {useParams} from "next/navigation";
+
+interface MenuSectionProps {
+    children: React.ReactNode;
+    label: string;
+}
+
+function MenuSection({children, label}: MenuSectionProps) {
+    return (
+        <label role="group">
+            <span>{label}</span>
+            <ul>{children}</ul>
+        </label>
+    );
+}
+
 
 export default function LocaleSwitcher() {
+
+    const router = useRouter();
+    const [isPending, startTransition] = useTransition();
+    const pathname = usePathname();
+    const params = useParams();
+    const createHandleMenuClick = (menuItem: string) => {
+        if (menuItem === locale) {
+            return;
+        }
+        return () => {
+            startTransition(() => {
+                router.replace(
+                    // @ts-expect-error -- TypeScript will validate that only known `params`
+                    // are used in combination with a given `pathname`. Since the two will
+                    // always match for the current route, we can skip runtime checks.
+                    {pathname, params},
+                    {locale: menuItem}
+                );
+            });
+        };
+    };
 
     const locale = useLocale();
 
     return (
-        <LocaleSwitcherSelect defaultValue={locale} label={'test'}>
-            {locales.map((cur) => (
-                <option key={cur} value={cur}>
-                    {cur}
-                </option>
-            ))}
-        </LocaleSwitcherSelect>
+        <Dropdown>
+            <MenuButton className="p-3 sm:text-base/7 text-sm">Язык</MenuButton>
+            <Menu className="bg-svoddWhite-400 dark:bg-svoddBlack-400 border rounded-xl p-3">
+                {locales.map((cur) => (
+                    <MenuItem key={cur} onClick={createHandleMenuClick(cur)}>
+                        {cur}
+                    </MenuItem>
+                ))}
+            </Menu>
+        </Dropdown>
     );
 }
