60af1509a3b4d8968dbcdc09d3e3e45625317285
agusev <audetv@gmail.com>
2024-04-09 20:13:53 +0300

    Added yandex metrika

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -6,6 +6,7 @@ import {locales} from '@/config';
 import {Providers} from "@/app/providers";
 import Navigation from "@/components/Navigation";
 import Footer from "@/components/Footer";
+import Script from "next/script";
 
 
 const inter = Inter({subsets: ['latin']});
@@ -31,6 +32,11 @@ export default async function LocaleLayout({
     return (
         <html className="dark h-full" lang={locale}>
         <body className={clsx(inter.className, `flex h-full flex-col bg-svoddWhite-400 dark:bg-svoddBlack-200`)}>
+        <Script id={"yandex-metrika"} strategy={"afterInteractive"}>
+            {
+                `<!-- Yandex.Metrika counter --> <script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date(); for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }} k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(96981294, "init", { defer: true, clickmap:true, trackLinks:true, accurateTrackBounce:true }); </script> <noscript><div><img src="https://mc.yandex.ru/watch/96981294" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->`
+            }
+        </Script>
         <Providers>
             <main>
                 <div className="container mx-auto lg:p-6 p-3 max-w-[1320px]">
