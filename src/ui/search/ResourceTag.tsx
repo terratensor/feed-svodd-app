9803089d2abd8860580229f3271ca866745c703b
agusev <audetv@gmail.com>
2024-04-09 18:07:04 +0300

    Исправлен текст тега в светлом режиме

--- src/ui/search/ResourceTag.tsx
+++ src/ui/search/ResourceTag.tsx
@@ -24,7 +24,7 @@ export default function ResourceTag({className, rid, name, active, handleClick, 
                 "bg-kremlin": rid == 1 && active,
                 "bg-mid": rid == 2 && active,
                 "bg-mil": rid == 3 && active,
-                "dark:bg-svoddGray-300 dark:text-svoddWhite-300 bg-gray-200 text-gray-700": !active &&(rid == 1 || rid == 2 || rid == 3)
+                "dark:bg-svoddGray-300 dark:text-svoddWhite-300 bg-gray-200 text-gray-800": !active &&(rid == 1 || rid == 2 || rid == 3)
             })}>{name.toUpperCase()}
             {showIcon ? <XMarkIcon className="w-4 h-4" /> : null}
         </span>
