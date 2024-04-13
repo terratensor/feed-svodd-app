80303e45e333b9229df1471364c5e2b4f3abade7
agusev <audetv@gmail.com>
2024-04-13 15:06:13 +0300

    Fixed language prefix in urls

--- src/ui/search/entry-title-url.tsx
+++ src/ui/search/entry-title-url.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
-import getURL from "@/utils/getURL";
+import getURL, {getLanguagePrefix} from "@/utils/getURL";
 import {getLocale} from "next-intl/server";
 
 export default async function EntryTitleUrl({title, url}: { title: string, url: string }) {
     const locale = await getLocale();
-    const link = getURL(`${locale}/entry?url=${url}`)
+    const link = getURL(`${getLanguagePrefix(locale)}/entry?url=${url}`)
     return (
         <Link href={link} prefetch={true}>
             <h3 className="text-xl font-semibold pt-8 mb-2">{title}</h3>
